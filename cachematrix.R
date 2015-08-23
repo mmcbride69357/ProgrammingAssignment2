0c363d67a08f368d5db9c17c33edbde721c44ed1
mmcbride69357 <school@wagepeace.org>
2015-08-23 13:31:25 -0500

    adjusted comments

--- cachematrix.R
+++ cachematrix.R
@@ -15,32 +15,9 @@
 
 # -----------------------------------------------------------------------------
 
-makeCacheMatrix <- function(x = matrix())
-{
-     ## Creates an object that contains a list and functions that:
-     # - Set the value of the matrix
-     # - Get the value of the matrix
-     # - Set the value of the inverted matrix
-     # - Get the value of the inverted matrix     
-     
-     m <- NULL
-     set <- function(y) {
-          x <<- y
-          m <<- NULL
-     }
-     get <- function() x
-     setmatrix <- function(matrix) m <<- matrix
-     getmatrix <- function() m
-     list(set = set, get = get,
-          setmatrix = setmatrix,
-          getmatrix = getmatrix)     
-}
-
-# -----------------------------------------------------------------------------
-
 cacheSolve <- function(x, ...) 
 {
-     ## Creates an object that:
+     ## Purpose:
      # - Takes a matrix and returns the matrix inverted.
      # - Stores the inverted matrix in cache.
      # - Returns the cached matrix the next time the function is called to avoid recomputation.
@@ -54,6 +31,29 @@ cacheSolve <- function(x, ...)
      m <- solve(data, ...)
      x$setmatrix(m)
      m     
+}
+
+# -----------------------------------------------------------------------------
+
+makeCacheMatrix <- function(x = matrix())
+{
+     ## Purpose:
+     # - Sets the value of the matrix 'x'
+     # - Gets the value of the matrix 'x'
+     # - Sets the value of the inverted matrix 'm'
+     # - Gets the value of the inverted matrix 'm'     
+     
+     m <- NULL
+     set <- function(y) {
+          x <<- y
+          m <<- NULL
+     }
+     get <- function() x
+     setmatrix <- function(matrix) m <<- matrix
+     getmatrix <- function() m
+     list(set = set, get = get,
+          setmatrix = setmatrix,
+          getmatrix = getmatrix)     
 }
 
 # -----------------------------------------------------------------------------
