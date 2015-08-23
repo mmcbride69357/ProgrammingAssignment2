20e5a14cfe25cf133bd97fce6c7f03af4a9cba0a
mmcbride69357 <school@wagepeace.org>
2015-08-23 13:17:00 -0500

    added code

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,59 @@
-### Put comments here that give an overall description of what your
-## functions do
+#####################################################################################################
+## Purpose:  Returns an inverted matrix given a square invertible matrix, and caches the result.
+##
+##  Example:
+##
+##  > source("cachematrix.R")
+##  > m = matrix(1:4, 2, 2)
+##  > cacheSolve(makeCacheMatrix(m))
+##
+##  Result:
+##
+##        [,1] [,2]
+##  [1,]   -2  1.5
+##  [2,]    1 -0.5
 
-## Write a short comment describing this function
+# -----------------------------------------------------------------------------
 
-makeCacheMatrix <- function(x = matrix()) {
-
+makeCacheMatrix <- function(x = matrix())
+{
+     ## Creates an object that contains a list and functions that:
+     # - Set the value of the matrix
+     # - Get the value of the matrix
+     # - Set the value of the inverted matrix
+     # - Get the value of the inverted matrix     
+     
+     m <- NULL
+     set <- function(y) {
+          x <<- y
+          m <<- NULL
+     }
+     get <- function() x
+     setmatrix <- function(matrix) m <<- matrix
+     getmatrix <- function() m
+     list(set = set, get = get,
+          setmatrix = setmatrix,
+          getmatrix = getmatrix)     
 }
 
+# -----------------------------------------------------------------------------
 
-## Write a short comment describing this function
-
-cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+cacheSolve <- function(x, ...) 
+{
+     ## Creates an object that:
+     # - Takes a matrix and returns the matrix inverted.
+     # - Stores the inverted matrix in cache.
+     # - Returns the cached matrix the next time the function is called to avoid recomputation.
+     
+     m <- x$getmatrix()
+     if(!is.null(m)) {
+          message("getting cached data")
+          return(m)
+     }
+     data <- x$get()
+     m <- solve(data, ...)
+     x$setmatrix(m)
+     m     
 }
+
+# -----------------------------------------------------------------------------
